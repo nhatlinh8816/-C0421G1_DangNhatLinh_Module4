1aab073303f105be92caeeb5b843b41e8b3d1dca
nhatlinh8816 <dangnhatlinh0213@gmail.com>
2021-09-08 09:55:50 +0700

    commit_b9_baitap_muon_sach_update_dem_luot_truy_cap

--- _AOP_Exception/exercise/book_manager/src/main/java/com/example/demo/config/LogAspect.java
+++ _AOP_Exception/exercise/book_manager/src/main/java/com/example/demo/config/LogAspect.java
@@ -3,10 +3,7 @@ package com.example.demo.config;
 import org.apache.tomcat.jni.Local;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -16,27 +13,40 @@ import java.time.LocalDateTime;
 @Aspect
 public class LogAspect {
     @Pointcut("execution(* com.example.demo.controller.BookController.update*(..))")
-    public void updateBookPointCut(){
+    public void updateBookPointCut() {
     }
 
-//    @Before("updateBookPointCut()")
-//    public void beforeCallMethodUpdate(JoinPoint joinPoint){
-//        System.err.println("Start method name: " +joinPoint.getSignature().getName()
-//        + " Time " + LocalDate.now());
+    @Pointcut("within(com.example.demo.controller.BookController)")
+    public void allMethodPointcut(){
+    }
+
+
+    @Before("updateBookPointCut()")
+    public void beforeCallMethodUpdate(JoinPoint joinPoint){
+        System.err.println("Start method name: " +joinPoint.getSignature().getName()
+        + " Time " + LocalDate.now());
+    }
+    //tạo biến count đếm số lượt truy cập
+    int count = 0;
+    @After("allMethodPointcut()")
+    public void afterAllMethod(JoinPoint joinPoint){
+        count++;
+        System.err.println("Visitor: " + count +" with method "+joinPoint.getSignature().getName());
+    }
+
+//    @Around("updateBookPointCut()")
+//    public Object aroundThrowingUpdateBook(ProceedingJoinPoint proceedingJoinPoint){
+//        System.err.println("Before method time "+ LocalDateTime.now());
+//        Object value = null;
+//
+//        try {
+//            value = proceedingJoinPoint.proceed();
+//        }catch (Throwable throwable){
+//            System.err.println("Throwing method time "+ LocalDateTime.now());
+//            throwable.printStackTrace();
+//        }
+//        System.err.println("After method time "+ LocalDateTime.now()+ "Return value = "+value);
+//        return value;
 //    }
 
-    @Around("updateBookPointCut()")
-    public Object aroundThrowingUpdateBook(ProceedingJoinPoint proceedingJoinPoint){
-        System.err.println("Before method time "+ LocalDateTime.now());
-        Object value = null;
-
-        try {
-            value = proceedingJoinPoint.proceed();
-        }catch (Throwable throwable){
-            System.err.println("Throwing method time "+ LocalDateTime.now());
-            throwable.printStackTrace();
-        }
-        System.err.println("After method time "+ LocalDateTime.now()+ "Return value = "+value);
-        return value;
-    }
 }
